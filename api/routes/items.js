dcd9e0d0c381bd6789d076763cbbe09058a9a1de
Cristian Fernandez <cristian.fernandez123@yahoo.com>
2022-01-15 15:40:37 -0800

    added node js to project

---
+++ api/routes/items.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+const User = require("../models/users");
+
+router.get("/users", async (req, res) => {
+  try {
+  } catch (e) {}
+});
+
+module.exports = router;
