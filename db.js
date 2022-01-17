cd28b8f9d00d2912f5a1591e236e0416dde8d3fb
Cristian Fernandez <cristian.fernandez123@yahoo.com>
2022-01-17 14:08:50 -0800

    testing db connection

--- db.js
+++ db.js
@@ -1,28 +1,12 @@
-const Pool = require("pg").Pool;
-require("dotenv").config();
+const { Client } = require("pg");
 
-const pool = new Pool({
-  user: "postgres",
-  password: "password",
-  host: "localhost",
-  port: 5432,
-  database: "Inventory",
-});
-
-const { Sequelize } = require("sequelize");
-
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
-  host: "localhost",
-  password: "password",
-  database: "Inventory",
-  port: 5432,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
   },
 });
 
-module.exports = sequelize;
+client.connect();
+
+module.exports = client;
