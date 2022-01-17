d19547e6f4063840d5108477010463bd113ce28f
Cristian Fernandez <cristian.fernandez123@yahoo.com>
2022-01-17 00:27:17 -0800

    added middleware

---
+++ api/middlewares/items/checkPayload.js
@@ -0,0 +1,21 @@
+const checkPayload = async (req, res, next) => {
+  try {
+    const { item_name, item_price, item_count, category } = req.body;
+
+    if (!item_name || !item_price || !category || item_count < 0) {
+      return res
+        .status(400)
+        .send("Please provide an item name, price, count, and category");
+    }
+
+    req.payload = req.body;
+
+    next();
+  } catch (error) {
+    res.status(500).send("test");
+  }
+};
+
+module.exports = {
+  checkPayload,
+};
--- api/routes/items.js
+++ api/routes/items.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const pool = require("../../db");
 
+// middlewares
+const { checkPayload } = require("../middlewares/items/checkPayload");
+
+// get all current items
 router.get("/items", async (req, res) => {
   try {
     const items = await pool.query("SELECT * FROM ITEMS");
@@ -11,11 +15,23 @@ router.get("/items", async (req, res) => {
   }
 });
 
-router.post("/items", async (req, res) => {
+// we need a item_name, item_price, item_count, category
+router.post("/items", [checkPayload], async (req, res) => {
   try {
+    const { item_name, item_price, item_count, category } = req.payload;
+
+    await pool.query(
+      `INSERT INTO items (item_name, item_price, item_count, category, created_on) VALUES ('${item_name}', '${item_price}', ${item_count},' ${category}', CURRENT_TIMESTAMP)`
+    );
+
+    res.status(201).send("Item added successfully");
   } catch (error) {
-    console.log(error);
+    res.status(500).send({ ERROR: error.message, DETAIL: error.detail });
   }
 });
+
+// delete
+
+// update item
 
 module.exports = router;
