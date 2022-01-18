e635f463158e680b0e860dc8375c584170401fa4
Cristian Fernandez <cristian.fernandez123@yahoo.com>
2022-01-17 21:56:25 -0800

    added filter button and add new item button

--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -1,12 +1,25 @@
 import React from "react";
 import "./searchbar.css";
 
+// icons
+import { BiFilter } from "react-icons/bi";
+import { RiAddFill } from "react-icons/ri";
+
 const SearchBar = () => {
   return (
     <ul className="searchbar-container">
       <form className="form-container">
         <input type="text" placeholder="Search Inventory..."></input>
       </form>
+      <div className="icon-container">
+        <button>
+          <BiFilter className="filter-icon" />
+        </button>
+        <button className="add-item">
+          <h2>Add new Item</h2>
+          <RiAddFill className="icon" />
+        </button>
+      </div>
     </ul>
   );
 };
